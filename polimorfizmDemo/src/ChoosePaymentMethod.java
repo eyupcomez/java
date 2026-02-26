9efa3c1f24240a9f9e6f638caa4b5f96a6ddbe59
eyupcomez <eyupcomez46@gmail.com>
2026-02-26 23:55:21 +0300

    new files added

---
+++ polimorfizmDemo/src/ChoosePaymentMethod.java
@@ -0,0 +1,2 @@
+public class ChoosePaymentMethod {
+}
--- polimorfizmDemo/src/Main.java
+++ polimorfizmDemo/src/Main.java
@@ -1,9 +1,20 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 
-        public static void main (String[] args){
-            CreditCardPayment payment = new CreditCardPayment();
-            payment.accepted();
-
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        try{
+            DbHelper helper = new DbHelper();
+            connection= helper.getConnection();
+            System.out.println("Connection Successfull!");
+        } catch (SQLException exception){
+            System.out.println(exception.getMessage());
         }
 
+
+
+
+    }
 }
