2e4c411952468ffc71e6502e8f2f0e01ef7e31ea
eyupcomez <eyupcomez46@gmail.com>
2026-02-26 16:30:50 +0300

    new project added

--- TaskManager/src/InputClass.java
+++ TaskManager/src/InputClass.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class InputClass {
+    Scanner scanner = new Scanner(System.in);
+
+    public void taskName() {
+        System.out.println("Please enter the task you want to add to your calender: ");
+        String taskName = scanner.nextLine();
+    }
 }
+
+
