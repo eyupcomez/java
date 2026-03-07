0f70e654efc3f0e7d031c0ac03d8e9f247609695
eyupcomez <eyupcomez46@gmail.com>
2026-03-07 03:24:40 +0300

    new changes added to project

--- compoundcalculator/src/Main.java
+++ compoundcalculator/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
         public static void main (String[] args){
+                VariableInput input = new VariableInput();
+                input.setInvestmentAmount();
+                input.getInvestmentAmount();
 
         }
 
--- compoundcalculator/src/VariableInput.java
+++ compoundcalculator/src/VariableInput.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class VariableInput {
+    private double investmentAmount = 0;
+    private double periodProfit = 0;
     Scanner scanner = new Scanner(System.in);
 
+    public void setInvestmentAmount(){
+        System.out.println("Please enter the amount of the investment: ");
+        this.investmentAmount = scanner.nextDouble();
+
+    }
+
+    public double getInvestmentAmount() {
+        return investmentAmount;
+    }
+
+    public void setPeriodProfit() {
+        System.out.println("Please enter the profit amount per period: ");
+        this.periodProfit = scanner.nextDouble();
+    }
+    public double getPeriodProfit(){
+        return periodProfit;
+    }
 }
