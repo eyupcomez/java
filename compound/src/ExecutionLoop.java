9a8266cc9d8dbb87957383df8f9db2459fca3b29
eyupcomez <eyupcomez46@gmail.com>
2026-03-08 02:23:42 +0300

    new version

--- compound/src/ExecutionLoop.java
+++ compound/src/ExecutionLoop.java
@@ -1,23 +1,34 @@
-public class ExecutionLoop extends VariableInput {
+public class ExecutionLoop  {
     VariableInput input;
 
     public ExecutionLoop(VariableInput input){
         this.input = input;
     }
     public void executeWhileLoop(){
+        double investmentAmount = input.getInvestmentAmount();
+        double baseProfit = input.getPeriodProfit();
+        double target = input.getTargetPeriodProfit();
         double incrementAmount = input.getPeriodProfit();
         double periodProfit = 0;
 
         System.out.println(incrementAmount);
+        double totalProfit = 0;
 
         int periods = 0;
+        int investments = 1;
 
-        while(periodProfit < input.getTargetPeriodProfit()){
-            periodProfit += input.getPeriodProfit();
+        while(totalProfit < target){
+            double currentPeriodProfit = baseProfit * investments;
+            totalProfit += currentPeriodProfit;
             periods++;
+            if (totalProfit >= investmentAmount * investments){
+                investments++;
+
+            }
         }
 
-        System.out.println("Total profit: " + periodProfit);
+        System.out.println("Total profit: " + totalProfit);
         System.out.println("Periods needed: " + periods);
+        System.out.println("Total investments: " + investments);
     }
 }
